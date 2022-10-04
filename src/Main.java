eaaebfb9db4d8d6b511c0fc6f13c7cb3d978bab6
Rayan Hadjari <rayan.ha@me.com>
2022-10-04 09:39:53 +0200

    finished

--- src/Main.java
+++ src/Main.java
@@ -9,8 +9,10 @@ public class Main {
         myFriend.SetHP(6000);
         myEnemy.SetHP(8000);
 
+        boolean loop = true;
+
         int Sandevistan = 0;
-            while (myFriend.hp > 0 || myEnemy.hp > 0) {
+            while (loop == true) {
                 System.out.println("\n" + myFriend.name + ": " + myFriend.hp);
                 System.out.println(myEnemy.name + ": " + myEnemy.hp);
                 System.out.println("\n" + myFriend.name + " has three moves: " + "\n" +
@@ -24,30 +26,39 @@ public class Main {
                             "\n" + "After delivering several blows David has dealt: 2000 DMG!" + "\n");
                     myEnemy.DMG(2000);
                     Sandevistan += 1;
+                    if (Sandevistan >= 3) {
+                        System.out.println(myFriend.name + " has used the Sandevistan too much." +
+                                "\n" + "David took 3000 DMG!" + "\n");
+                        myFriend.DMG(3000);
+                    }
+                    myEnemy.GetHP();
+                    myFriend.GetHP();
                 }
-                if (newChoice == 2) {
+                else if (newChoice == 2) {
                     System.out.println("\n" + myFriend.name + " unleashes a Flurry of Blows!" +
                             "\n" + "David dealt: 500 DMG!" + "\n");
                     myEnemy.DMG(500);
+                    myEnemy.GetHP();
+                    myFriend.GetHP();
                 }
-                if (newChoice == 3) {
+                else if (newChoice == 3) {
                     System.out.println("\n" + myFriend.name + " tries to charm " + myEnemy.name + "..." + "\n" +
                             "It had no effect since " + myEnemy.name + " is an emotionless killing machine.");
                 }
-                if (Sandevistan == 3) {
-                    System.out.println(myFriend.name + " has used the Sandevistan too much." +
-                            "\n" + "David took 3000 DMG!");
-                    myFriend.DMG(3000);
-                }
-                System.out.println(myEnemy.name + " DUNKS on " + myFriend.name + " with the strength of an unstoppable titan!" +
+                System.out.println("After " + myFriend.name + " has landed his hit, " + myEnemy.name + " gets ready and DUNKS on " + myFriend.name + " with the strength of an unstoppable titan!" +
                         "\n" + myFriend.name + " is able to block in time but still took: 1000 DMG!" + "\n");
                 myFriend.DMG(1000);
+                myEnemy.GetHP();
+                myFriend.GetHP();
+                if(myFriend.GetHP() <= 0 || myEnemy.GetHP() <= 0){
+                    loop = false;
+                }
             }
         if(myFriend.GetHP() <= 0){
-            System.out.println("\n" + myFriend.name + " died");
+            System.out.println(myFriend.name + " died");
         }
         else if (myEnemy.GetHP() <= 0){
-            System.out.println("\n" + myFriend.name + " beat Adam Smasher!!");
+            System.out.println(myFriend.name + " beat Adam Smasher!!");
         }
     }
 }
